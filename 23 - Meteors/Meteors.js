9fc03e0b39600f955ab5dcccdc3c336b9b8720aa
Cannnto <gabrielcantotdg517@gmail.com>
2025-03-06 14:43:57 -0300

    drawMeteor

--- 23 - Meteors/Meteors.js
+++ 23 - Meteors/Meteors.js
@@ -50,12 +50,28 @@ class meteor extends Entity{
     {
         super(0, y, 20+Math.random()*canvas.width/20, height);
         this.img.src = "meteorito.png"
-        this.speed = 1
+        this.speed = 1;
+        this.mag = 20;
+        this.color = 55+Math.random()*200;
+        this.drawRandow = 30+Math.random()*90;
+    }
+
+    draw()
+    {   // context.fillStyle = 'green';
+        // context.fillRect(this.x, this.y,this.width,this.height);
+        
+        context.fillStyle = `rgb(${this.color},${this.color},${this.color})`;
+        context.beginPath();
+        context.moveTo((this.x+this.width/2)+Math.cos(0)*this.width/2, (this.y+this.height/2)+Math.sin(0)*this.height/2);
+        for(let i=1*Math.PI/180; i<Math.PI*2; i+=this.drawRandow*Math.PI/180)
+            context.lineTo((this.x+this.width/2)+Math.cos(i)*this.width/2, (this.y+this.height/2)+Math.sin(i)*this.height/2);
+        context.closePath();
+        context.fill();
     }
 
     update()
     {
-        this.y += this.speed
+        this.y += this.speed;
     }
 }
 
